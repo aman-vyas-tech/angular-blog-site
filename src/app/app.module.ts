f9a53d59fe984707e575352ccad48c001216a537
Aman Vyas <1557282@TCS.com>
2020-05-02 00:07:12 +0530

    Added admin user to app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -22,11 +22,13 @@ import { BlogComponent } from './components/blog/blog.component';
 import { PaginatorComponent } from './components/paginator/paginator.component';
 
 import { AuthGuard } from './guards/auth.guard';
+import { AdminAuthGuard } from './guards/admin-auth.guard';
+import { AuthorProfileComponent } from './components/author-profile/author-profile.component';
 
 const routes = [
   { path: '', component: HomeComponent, pathMatch: 'full' },
   { path: 'blog/:id/:slug', component: BlogComponent },
-  { path: 'addpost', component: BlogEditorComponent, canActivate: [AuthGuard] },
+  { path: 'editpost/:id', component: BlogEditorComponent, canActivate: [AdminAuthGuard] },
   { path: 'editpost/:id', component: BlogEditorComponent },
   { path: 'page/:pagenum', component: HomeComponent },
   { path: '**', component: HomeComponent },
@@ -43,6 +45,7 @@ const routes = [
     BlogCardComponent,
     BlogComponent,
     PaginatorComponent,
+    AuthorProfileComponent,
   ],
   imports: [
     BrowserModule,
